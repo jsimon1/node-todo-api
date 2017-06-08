3dd7151702a5189ae34475c669acdc3732dc6877
Jeremy Simon <jeremysimon2@gmail.com>
2017-06-08 09:54:37 -0400

    Initial commit - seeting up Users and Todos

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,65 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// Object destructuring
+// var user = {name: 'Jeremy', age: 25};
+// var {name} = user;
+// var obj = new ObjectID();
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
+  // Instead of if else, you can return for the error to handle it at the beginning, and have it end there
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, res) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(res.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Jeremy Simon',
+    age: 19,
+    location: 'Montvale'
+  }, (err, res) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+
+    console.log(res.ops[0]._id.getTimestamp());
+  });
+
+  // MongoDB query
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.collection('Users').find().toArray().then((users) => {
+    console.log('Users:');
+    console.log(JSON.stringify(users, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+  //
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5938d96271a3b653d08f5b5a')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db. close();
+
+});
