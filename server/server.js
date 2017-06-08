b44ff4434f374fddf90d5a2de265eab5c471becc
Jeremy Simon <jeremysimon2@gmail.com>
2017-06-08 18:00:27 -0400

    Made DB functionaltiy more modular, added POST functionality

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+// Bodyparser used as Express middleware to parse the request body
+app.use(bodyParser.json());
+
+// CRUD - Create, Read, Update, Delete
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
